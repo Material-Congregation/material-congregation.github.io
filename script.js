230d22679ee69abedf7f7eaea6b326341e67532a
Interfiber <webmaster@interfiber.dev>
2024-02-16 19:59:52 -0500

    cattpuccin + update style

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mainSidenav").style.width = "250px";
     document.getElementById("main").style.filter = "blur(10px)";
 }
   
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mainSidenav").style.width = "0";
     document.getElementById("main").style.filter = "blur(0px)";
 } 
